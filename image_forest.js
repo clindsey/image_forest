1ef34ffe9cbd5eb3afd29663e45d5d52c5fc91bc
Chris Lindsey <chrislindsey218@gmail.com>
2011-08-31 16:16:17 -0700

    saving

--- image_forest.js
+++ image_forest.js
@@ -24,8 +24,8 @@
         hero.sprite_index += 1;
         if(hero.sprite_index > hero.sprites[hero.direction].walk.length - 1){
           hero.sprite_index = 0;
-        };
-      };
+        }
+      }
       tile_manager.render();
     },20);
     jQuery(document).keydown(function(e){
@@ -37,31 +37,31 @@
           hero.direction = 'northwest';
           if(map.get_tile(hero.map_x - 1,hero.map_y)){
             vx -= 1;
-          };
-        };
+          }
+        }
         if(e.keyCode === 38){
           hero.sprite = hero.sprites.northeast.stop;
           hero.direction = 'northeast';
           if(map.get_tile(hero.map_x,hero.map_y - 1)){
             vy -= 1;
-          };
-        };
+          }
+        }
         if(e.keyCode === 39){
           hero.sprite = hero.sprites.southeast.stop;
           hero.direction = 'southeast';
           if(map.get_tile(hero.map_x + 1,hero.map_y)){
             vx += 1;
-          };
-        };
+          }
+        }
         if(e.keyCode === 40){
           hero.sprite = hero.sprites.southwest.stop;
           hero.direction = 'southwest';
           if(map.get_tile(hero.map_x,hero.map_y + 1)){
             vy += 1;
-          };
-        };
+          }
+        }
         hero.move_by(vx,vy);
-      };
+      }
     });
   };
   var TileManager = function(hero,map,context){
@@ -80,9 +80,9 @@
     var tile_cache = {};
     var sprite_tilemap = SpriteMap(sprite,0,0,315,128),
         tree_tilemap = SpriteMap(sprite,315,0,70,116);
-    tile_cache['grass'] = Tile(sprite_tilemap,4,w,h);
-    tile_cache['tree_grass'] = Tile(sprite_tilemap,2,w,h);
-    tile_cache['tree'] = Tile(tree_tilemap,0,tree_tilemap.width,tree_tilemap.height);
+    tile_cache.grass = Tile(sprite_tilemap,4,w,h);
+    tile_cache.tree_grass = Tile(sprite_tilemap,2,w,h);
+    tile_cache.tree = Tile(tree_tilemap,0,tree_tilemap.width,tree_tilemap.height);
     self.render = function(){
       context.clearRect(0,0,canvas_width,canvas_height);
       var offset_x = hero.map_x - Math.floor(width / 2),
@@ -97,22 +97,22 @@
           if(tile_x < 0 - w || tile_y < 0 - h || tile_x > canvas_width + w || tile_y > canvas_height + h){
             // only draw visible tiles
             continue;
-          };
+          }
           var tile_index = map.get_tile(x + offset_x,y + offset_y);
           if(tile_cache[tile_index] === undefined){
             tile_cache[tile_index] = Tile(sprite,tile_index,w,h);
-          };
+          }
           if(tile_index === 0){
-            context.drawImage(tile_cache['tree_grass'],tile_x,tile_y);
-            context.drawImage(tile_cache['tree'],tile_x - 3.5,tile_y - tree_tilemap.height + h / 1.5);
+            context.drawImage(tile_cache.tree_grass,tile_x,tile_y);
+            context.drawImage(tile_cache.tree,tile_x - 3.5,tile_y - tree_tilemap.height + h / 1.5);
           }else{
-            context.drawImage(tile_cache['grass'],tile_x,tile_y);
-          };
+            context.drawImage(tile_cache.grass,tile_x,tile_y);
+          }
           if(hero.map_x === x + offset_x && hero.map_y === y + offset_y){
             context.drawImage(hero.sprite,tile_x + 22,tile_y - hero.sprite.height + h / 1.5);
-          };
-        };
-      };
+          }
+        }
+      }
     };
     return self;
   };
@@ -152,21 +152,21 @@
     self.sprites.northeast.walk = AnimationFrames(character_tilemap,w,h,index,24,2);
     // southeast
     index = 1;
-    var canvas = TmpCanvas(w,h);
+    canvas = TmpCanvas(w,h);
     canvas.context.drawImage(character_tilemap,0,h * index,w,h,0,0,w,h);
     self.sprites.southeast = {};
     self.sprites.southeast.stop = canvas.canvas;
     self.sprites.southeast.walk = AnimationFrames(character_tilemap,w,h,index,24,2);
     // southwest
     index = 2;
-    var canvas = TmpCanvas(w,h);
+    canvas = TmpCanvas(w,h);
     canvas.context.drawImage(character_tilemap,0,h * index,w,h,0,0,w,h);
     self.sprites.southwest = {};
     self.sprites.southwest.stop = canvas.canvas;
     self.sprites.southwest.walk = AnimationFrames(character_tilemap,w,h,index,24,2);
     // northwest
     index = 3;
-    var canvas = TmpCanvas(w,h);
+    canvas = TmpCanvas(w,h);
     canvas.context.drawImage(character_tilemap,0,h * index,w,h,0,0,w,h);
     self.sprites.northwest = {};
     self.sprites.northwest.stop = canvas.canvas;
@@ -182,7 +182,7 @@
       var c = TmpCanvas(w,h);
       c.context.drawImage(tilemap,(i + 1) * pixel_height,h * index,w,h,0,0,w,h);
       frames[i] = c.canvas;
-    };
+    }
     return frames;
   };
   var SpriteMap = function(img,x,y,w,h){
